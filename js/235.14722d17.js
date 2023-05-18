06337a66471c176ab4192223f487be658f042e9d
andreyko21 <andriy.zakharkiv.2001@gmail.com>
2023-05-18 18:05:18 +0300

    version 0.5

---
+++ js/235.14722d17.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkkinocms"]=self["webpackChunkkinocms"]||[]).push([[235],{3235:function(n,r,u){u.r(r),u.d(r,{default:function(){return a}});var e=u(3396);function c(n,r,u,c,t,i){return(0,e.wg)(),(0,e.iD)("div",null,"Розсилки")}var t={},i=u(89);const s=(0,i.Z)(t,[["render",c]]);var a=s}}]);
+//# sourceMappingURL=235.14722d17.js.map
